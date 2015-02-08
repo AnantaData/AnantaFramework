ef8fe40d534ecf8d49c0657cb6d12af5a41714ec
Lakmal <tmtlakmal@gmail.com>
2015-02-08 23:23:47 +0530

    Added som to setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='AnantaFramework',
     version='0.0.1',
     packages=['ananta_gui', 'ananta_base', 'ananta_base.mining', 'ananta_base.mining.clustering',
-              'ananta_base.mining.clustering.gsom', 'ananta_base.mining.clustering.kgsom',
+              'ananta_base.mining.clustering.gsom', 'ananta_base.mining.clustering.kgsom','ananta_base.mining.clustering.som',
               'ananta_base.mining.clustering.fusion_gsom', 'ananta_views', 'ananta_profile'],
     url='',
     license='',
