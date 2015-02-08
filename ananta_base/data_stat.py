a3e4e26788780cccadd06f96b5ea6211c9996e63
Lakmal <tmtlakmal@gmail.com>
2015-02-08 17:12:57 +0530

    Added changes to getStatistics

--- ananta_base/data_stat.py
+++ ananta_base/data_stat.py
@@ -1,7 +1,6 @@
 __author__ = 'lakmal'
 import shutil
-
-
+import pandas as pd
 
 def getStatistics(trainingSet, filename_prefix):
     print "Started tracking Statistics and Data"
@@ -16,15 +15,23 @@ def getStatistics(trainingSet, filename_prefix):
     to_file = open(stat_save,"w");
     to_file.write("Field,Count,Mean,St.Dev,Min,Q1,Median,Q3,Max\n")
     shutil.copyfileobj(from_file, to_file)
+    from_file.close()
     to_file.close()
     shutil.copyfile(stat_save,"stat.csv")
     print "Finished tracking Statistics"
 
+    types = pd.DataFrame({'DataType':trainingSet.data.dtypes})
+    types.to_csv("types_temp.csv", sep=",", encoding="utf-8")
+
+    from_file = open("types_temp.csv","r");
+    from_file.readline() # and discard
     types_save = str(filename_prefix)+"types.csv"
-    df = trainingSet.data
-    types_list = df.dtypes
-    types_list.to_csv(types_save, sep=",", encoding="utf-8")
-    types_list.to_csv("types.csv", sep=",", encoding="utf-8")
+    to_file = open(types_save,"w");
+    to_file.write("Field,DataType\n")
+    shutil.copyfileobj(from_file, to_file)
+    from_file.close()
+    to_file.close()
+    shutil.copyfile(types_save,"types.csv")
     print "Finished tracking DataTypes"
 
     data_save = str(filename_prefix)+"data.csv"
