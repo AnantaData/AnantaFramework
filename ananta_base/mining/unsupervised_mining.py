56e2574526c49694f13349c745eedbabae3e8087
damith senanayake <dracusds123@gmail.com>
2015-02-12 23:29:49 +0530

    changed the KGSOM code

--- ananta_base/mining/unsupervised_mining.py
+++ ananta_base/mining/unsupervised_mining.py
@@ -104,7 +104,7 @@ class KGSOMStep:
 
     def __init__(self,dimensions):
         self.dims = dimensions
-        self.mp = kgsom.gsomap(SP=0.9999,dims=self.dims,nr_s=10,lr_s=0.01,fd=0.999,lrr=0.95,sig2=1000,prune=0.8)
+        self.mp = kgsom.gsomap(SP=0.999999,dims=self.dims,nr_s=10,lr_s=0.01,fd=0.999,lrr=0.95,sig2=100000,prune=0.75)
 
     def execute(self,data):
         data = np.array(data)
